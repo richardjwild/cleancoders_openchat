821ee5ad9f2dd24c469756a7ad02843048d86980
Richard Wild <richardwild76@gmail.com>
2018-08-28 17:23:13 +0100

    Return the first post text

--- src/main/java/org/openchat/api/UsersAPI.java
+++ src/main/java/org/openchat/api/UsersAPI.java
@@ -1,11 +1,23 @@
 package org.openchat.api;
 
 import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
 
 public class UsersAPI {
 
+    private String text;
+
     public String retrievePosts() {
         JsonArray json = new JsonArray();
+        if (text != null) {
+            JsonObject post = new JsonObject();
+            post.add("text", text);
+            json.add(post);
+        }
         return json.toString();
+    }
+
+    public void createPost(String text) {
+        this.text = text;
     }
 }
--- src/test/java/org/openchat/api/Users_API_should.java
+++ src/test/java/org/openchat/api/Users_API_should.java
@@ -1,21 +1,37 @@
 package org.openchat.api;
 
 import org.json.JSONException;
+import org.junit.Before;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
 public class Users_API_should {
 
-    public static final boolean STRICT = true;
+    private static final boolean STRICT = true;
+
+    private UsersAPI usersAPI;
+
+    @Before
+    public void setup() {
+        usersAPI = new UsersAPI();
+    }
 
     @Test
     public void return_empty_list_of_messages_when_none_posted() throws JSONException {
-        UsersAPI usersAPI = new UsersAPI();
-        String expected = "[]";
-
         String actual = usersAPI.retrievePosts();
 
+        String expected = "[]";
         JSONAssert.assertEquals(expected, actual, STRICT);
     }
 
+    @Test
+    public void return_the_message_when_one_has_been_posted() throws JSONException {
+        String firstPostText = "first post!";
+        usersAPI.createPost(firstPostText);
+
+        String actual = usersAPI.retrievePosts();
+
+        String expected = "[{\"text\":\"" + firstPostText + "\"}]";
+        JSONAssert.assertEquals(expected, actual, STRICT);
+    }
 }
